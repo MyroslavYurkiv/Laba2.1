6b558545055c1e8caa1bdc02d641adcd17194872
Yurkivy <myroslav-petro.yurkiv.itis.2020@lpnu.ua>
2020-10-14 22:33:56 +0300

    first

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double a; 
+	double z1; 
+	//double z2; 
+	cout << "a = "; cin >> a;
+	z1 = (1 - 2 * (pow(sin(a),2))) / (1 + sin(2 * a)); 
+	//z2 = (1 - tan(a)) / (1 + tan(a));
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
